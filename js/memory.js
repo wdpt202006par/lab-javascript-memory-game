b8e35caffebe520cce273236dc1b8351e532434d
toufiq23 <drrameraaron023@gmail.com>
2020-06-20 16:37:47 +0200

    Lab Practice

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,14 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+		// add the rest of the class properties here
+		
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+  shuffleCards() {
+
+	}
+  checkIfPair(card1, card2) {
+
+	}
   isFinished() {}
 }
