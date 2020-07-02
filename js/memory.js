762ba6505e7589805ac144d4282cb55a0f01fcbf
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-07-02 21:35:12 +0200

    IT 1-2 done

--- js/memory.js
+++ js/memory.js
@@ -5,8 +5,30 @@ class MemoryGame {
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
+    
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    this.cards.sort(function (a, b) {
+      return Math.random()-0.5 // [0-----1] -.5 -> [-.5-----.5]
+    })
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if(card1 === card2){
+      this.pairsGuessed += 1;
+       return true; 
+    } else {
+       return false;
+    }
+  }
+
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
