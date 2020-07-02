3970cdc98be2e8389e00c5ec6c62a683765a766f
abernier <antoine.bernier@gmail.com>
2020-07-02 20:31:28 +0200

    solution

--- js/index.js
+++ js/index.js
@@ -39,11 +39,62 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+  function showCard($card) {
+    $card.classList.add('turned')
+  }
+  function hideCard($card) {
+    $card.classList.remove('turned')
+  }
+
+  function cardName($card) {
+    return $card.dataset.cardName;
+  }
+
+  let $playingCard; // `$playingCard` is the card we are trying to find
+  document.querySelectorAll('.card').forEach($card => {
+    $card.onclick = function() {
+      console.log('Card clicked: ', $card);
+
+      // We turn over the card in any case
+      showCard($card);
+
+      if ($playingCard) {
+        // We compare our `card` with `$playingCard`
+        if (memoryGame.checkIfPair(cardName($card), cardName($playingCard))) {
+          console.log('yes!');
+        } else {
+          console.log('no!');
+
+          //
+          // Let's hide to 2 cards in 0.5s
+          //
+          let $playingCardCopy = $playingCard; // we make a copy because `$playingCard` is about to be `undefined` and will in 1000ms
+          setTimeout(function () {
+            hideCard($playingCardCopy);
+            hideCard($card);
+          }, 500);
+        }
+
+        $playingCard = undefined; // we reset `$playingCards`
+      } else {
+        // let's define our new `$playingCard` (the one we will try to find the next click)
+        $playingCard = $card;
+      }
+
+      //
+      // re-render score
+      //
+
+      document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;
+      document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+
+      //
+      //
+      //
+
+      if (memoryGame.isFinished()) {
+        alert('You win!');
+      }
+    };
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
+    this.shuffleCards();
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.sort(function (a, b) {
+      return Math.random() - 0.5;
+    });
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+
+    return false;
+  }
+  isFinished() {
+    return this.pairsGuessed >= (this.cards.length /2);
+  }
 }
