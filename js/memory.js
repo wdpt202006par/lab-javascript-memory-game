01d4bc56509ff0b44badc2e351cb036327f8e878
Claire Lacanal <lacanal.claire@gmail.com>
2020-07-02 22:49:44 +0200

    Iteration 1 and Iteration 2 DONE

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    // melanger : trier aleatoiremetn
+
+    this.cards.sort(function (a, b) {
+      return Math.random() - 0.5; // [0-----1] -> [-.5-----.5]
+    })
+  }
+
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++; //quand on va cliquer, ça va rajouter +1
+    if (card1 === card2) {
+      this.pairsGuessed += 1; // On a trouvé une paire
+    }
+    return card1 === card2; // Va retourner vrai si c'est les mêmes
+  }
+
+
+  isFinished() {
+    let begin = false;
+    if (this.pairsGuessed === this.cards.length / 2) {
+      begin = true;
+    } else {
+      begin = false;
+    }
+    return begin;
+  }
+
 }
