d4e8e3417316d1c5971c249efed9b220596a31e9
Claire Lacanal <lacanal.claire@gmail.com>
2020-07-03 18:06:27 +0200

    Dom ongoing

--- js/index.js
+++ js/index.js
@@ -39,11 +39,46 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  function showCard($card) {
+    $card.classList.add('turned')
+  }
+  function hideCard($card) {
+    $card.classList.remove('turned')
+  }
+  function cardName($card) {
+    return $card.dataset.cardName;
+  }
+
   // Bind the click event of each element to a function
+  let playingCard;
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+            // We turn over the card in any case
+            if (playingCard) {
+              // We compare our `card` with `$playingCard`
+              if (memoryGame.checkIfPair(cardName(card), cardName(playingCard))) {
+                console.log('yes!');
+              } else {
+                console.log('no!');
+                //
+                // Let's hide to 2 cards in 0.5s with setTimeout()
+              }
+              playingCard = undefined; // we reset `playingCards`
+            } else {
+              // let's define our new `playingCard` (the one we will try to find the next click)
+              playingCard = card;
+            }
+            //
+            // re-render score
+            //
+            //
+            //
+            //
+            if (memoryGame.isFinished()) {
+              alert('You win!');
+            }
     });
   });
 });
