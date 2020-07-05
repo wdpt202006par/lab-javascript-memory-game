82533028e3fbd563fe25fb53df4afc2b67e26683
marwa.brj@gmail.com <marwa.brj@gmail.com>
2020-07-05 17:21:02 +0200

    affichage done

--- js/memory.js
+++ js/memory.js
@@ -2,13 +2,43 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
 		// add the rest of the class properties here
-		
+		this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards() {
+		// let i = this.cards.length;
+		// let randomCard;
+		// let shuffleCards;
 
+		// while (--i > 0) {
+		// 	randomCard = Math.floor(Math.random() * (i));
+		// 	shuffleCards = this.cards[randomCard];
+		// 	this.cards[randomCard] = this.cards[i];
+		// 	shuffleCards = this.cards[i];
+		// }
+
+    //
+
+    this.cards.sort(function (a, b) {
+      return Math.random() - 0.5; // [-.5--0---.5]
+    })
 	}
   checkIfPair(card1, card2) {
-
+		this.pairsClicked ++;
+    if(card1 === card2){
+      this.pairsGuessed ++;
+      return true
+    }else {
+      return false
+    }
 	}
-  isFinished() {}
+  isFinished() {
+    // if (this.pairsGuessed=== (this.cards.length)/2){
+    // return true
+    // } else {
+    //    return false
+		// }
+		return (this.pairsGuessed=== (this.cards.length)/2);
+  }
 }
